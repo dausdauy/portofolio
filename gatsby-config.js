cc73856b413eadc8a34dc04d157c23ceb0eb2d3d
daus dauy <dausdauy@gmail.com>
2021-01-29 16:47:23 +0700

    cancel

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: `2268584930`,
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
       },
     },
     {
